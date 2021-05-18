63205649411352dbbc4d00401b3275541eea2565
Pedro Massango <pedromassango.developer@gmail.com>
2021-05-18 12:41:53 +0100

    Added kotlinx dependencies for json de-serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id ("org.jetbrains.kotlin.plugin.serialization") version "1.5.0-RC"
 }
 
 group = properties("pluginGroup")
@@ -30,6 +31,8 @@ repositories {
 }
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc-218")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.0")
 }
 
 // Configure gradle-intellij-plugin plugin.
